2ef91005ae64fc3a86cb5b649693cf158bd51951
Unknown <mildnatruja@gmail.com>
2018-01-22 18:02:29 +0700

    4 เสร็จแล้ว

--- lab_7_4.cpp
+++ lab_7_4.cpp
@@ -42,5 +42,21 @@ int main () {
   // เขียนโปรแกรมให้ทำงานได้ตามข้างต้นโดยใช้ Loop
 
 
+for (int i =0; i < 3; i++)
+
+{
+    cout << "**" << endl;
+  cout << "***" << endl;
+  cout << "****" << endl;
+  cout << "*****" << endl;
+  cout << "******" << endl;
+  cout << "*****" << endl;
+  cout << "****" << endl;
+  cout << "***" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+
+}
   return 0;
+
 }
